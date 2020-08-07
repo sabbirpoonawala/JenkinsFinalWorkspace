c6e6d6dfe10b84e0ab0907999982ddad587e4125
sabbir <sabbir@sabbir-PC>
2020-08-07 11:47:20 +0530

    Test cases included

---
+++ SpringHelloWorld/src/test/java/com/yash/main/TestHelloController.java
@@ -0,0 +1,31 @@
+package com.yash.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.yash.controller.HelloController;
+
+class TestHelloController {
+	
+	private HelloController controller;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		controller=new HelloController();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		controller=null;
+	}
+
+	@Test
+	void testSayHello() {
+       String actual=controller.sayHello();
+       assertEquals("HelloWorld",actual);
+	}
+
+}
